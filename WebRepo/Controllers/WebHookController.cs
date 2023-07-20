233cdbc9ad7d553ee4331d2674cd5a63701046e1
Subhiksha Kumaresan <subhiksha.kumaresan@claysys.co>
2023-07-20 15:25:37 +0530

    WebHook Code

---
+++ WebRepo/Controllers/WebHookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebRepo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebHookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> WebhookHandlerAsync()
+        {
+            try
+            {
+                // Log the request body
+                using (StreamReader reader = new StreamReader(Request.Body))
+                {
+                    string requestBody = await reader.ReadToEndAsync();
+                    Console.WriteLine("Received Payload: " + requestBody);
+                }
+
+                // Process the webhook payload here
+                // Add your logic to handle the received payload
+
+                // Example: Return a response
+                return Ok("Webhook received successfully.");
+            }
+            catch (Exception ex)
+            {
+                // Log any exceptions that occurred during webhook processing
+                Console.WriteLine("Exception: " + ex.Message);
+                return BadRequest("Error processing webhook: " + ex.Message);
+            }
+        }
+
+    }
+}
---
+++ WebRepo/Models/WebHook.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRepo.Models
+{
+    public class WebHook
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
